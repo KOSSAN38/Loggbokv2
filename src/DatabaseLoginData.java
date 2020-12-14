dddef66b93c3ab5bcf053290f6703829684d1a30
isak.edlund <isak.edlund@elev.ga.ntig.se>
2020-12-14 11:04:58 +0100

    Added database code from magnus

---
+++ src/DatabaseLoginData.java
@@ -0,0 +1,8 @@
+public class DatabaseLoginData {
+    static String user = "isak1";
+    static String password = "isak1";
+    static String port = "3306";
+    static String DBname = "Loggbok";
+    static String DBURL = "localhost";
+}
+
